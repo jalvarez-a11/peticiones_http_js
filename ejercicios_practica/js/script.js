c22e195dd085e74c915fff8a947d237c41623fde
jalvarez-a11 <139930695+jalvarez-a11@users.noreply.github.com>
2024-07-06 08:52:29 -0500

    Add files via upload

--- ejercicios_practica/js/script.js
+++ ejercicios_practica/js/script.js
@@ -8,3 +8,44 @@
 /* Enunciado
     --> Leer el README para ver el enunciado
 */
+const personajeInput = document.getElementById('personaje');
+const consultarButton = document.getElementById('btnConsultar');
+const characterInfoDiv = document.getElementById('login'); 
+
+consultarButton.addEventListener('click', () => {
+  const personaje = personajeInput.value.trim();
+  if (!personaje) return;
+
+  const url = `https://rickandmortyapi.com/api/character/?name=${personaje}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      if (data.info.count === 0) {
+        characterInfoDiv.innerHTML = '<p>Personaje no encontrado.</p>';
+        return;
+      }
+
+      displayCharacterInfo(data.results[0]);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      characterInfoDiv.innerHTML = '<p>Error al buscar el personaje.</p>';
+    });
+});
+
+function displayCharacterInfo(character) {
+    const characterHTML = `
+      <h2>${character.name}</h2>
+      <img src="${character.image}" alt="${character.name}">
+      <ul>
+        <li>Especie: ${character.species}</li>
+        <li>Género: ${character.gender}</li>
+        <li>Origen: ${character.origin.name}</li>
+        <li>Estado: ${character.status}</li>
+      </ul>
+    `;
+    characterInfoDiv.innerHTML = '';L
+    characterInfoDiv.innerHTML = characterHTML;
+  }
+  console.log(character); 
